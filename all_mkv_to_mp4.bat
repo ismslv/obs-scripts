9028f40968535b8f598edee0d4e42807184509a5
ismslv <37056221+ismslv@users.noreply.github.com>
2022-09-03 07:13:40 +0200

    Update all_mkv_to_mp4.bat

--- all_mkv_to_mp4.bat
+++ all_mkv_to_mp4.bat
@@ -1,2 +1,9 @@
 @echo off
-for %%f in (*.mkv) do ffmpeg -i "%%~f" -codec copy "%%~nf.mp4"
+for %%f in (*.mkv) do (
+	echo Converting %%~f to %%~nf.mp4...
+	ffmpeg -i "%%~f" -codec copy "%%~nf.mp4"
+	echo %%~nf.mp4 created!
+	echo Moving %%~f to recycle bin.
+	recycle "%%~f"
+  REM Using http://www.maddogsw.com/cmdutils/
+)
